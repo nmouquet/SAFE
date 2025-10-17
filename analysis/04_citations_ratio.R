d6916fdf0b856cadd136fdfaf5d4441d1bdcda77
nmouquet <nicolas.mouquet@cnrs.fr>
2025-10-17 12:06:17 +0200

    adding citation ratio stufs

--- analysis/04_citations_ratio.R
+++ analysis/04_citations_ratio.R
@@ -6,5 +6,11 @@ doi_list <- unlist(refs$"doi")
 names(doi_list) <- NULL
 cleaned_doi <- gsub("https://doi.org/", "", doi_list)
 cleaned_doi <- gsub("doi:", "", cleaned_doi)
-faircitations::citation_ratio(cleaned_doi)
+fp_article_fairness <- fairpub::fp_compute_ratio(cleaned_doi)
+
+list_of_dois <- cleaned_doi
+lapply(list_of_dois, fairpub::fp_article_fairness) |>
+  do.call(rbind.data.frame)
+
+fairpub::fp_article_fairness(cleaned_doi[1])
 
